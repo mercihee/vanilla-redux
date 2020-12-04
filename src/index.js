5145b004175a3fa6ccec9b0ded657e34972226de
kheejin <heemerci@gmail.com>
2020-12-04 14:52:44 +0900

    change the count value using the reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import React from 
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const reducer = (count = 0, action) => {
+  if (action.type === "add") {
+    return count + 1;
+  } else if (action.type === "minus") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
+const store = createStore(reducer);
+
+const onchange = () => {
+  number.innerText = store.getState();
+};
+
+store.subscribe(onchange);
+
+const handleAdd = () => {
+  store.dispatch({ type: "add" });
+};
+
+const handleMinus = () => {
+  store.dispatch({ type: "minus" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
